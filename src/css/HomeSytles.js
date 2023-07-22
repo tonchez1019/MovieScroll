84dfa5449e380d81559d40b0317cffdc29067454
Tonchez <tonchezalonso@hotmail.com>
2023-07-21 21:01:01 -0600

    se agrega archivo de Css de HomeScreen

---
+++ src/css/HomeSytles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const HomeStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+   
+})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { HomeStyles } from '../css/HomeSytles';
+
+
 const HomeScreen = () => {
     return (
-        <View style={styles.container}>
+        <View style={HomeStyles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
