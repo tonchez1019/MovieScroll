87d857c89066f17a34d171fff957eb55d270b264
Tonchez <tonchezalonso@hotmail.com>
2023-07-21 20:43:58 -0600

    .

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+export default HomeScreen
+
+const styles = StyleSheet.create({})
